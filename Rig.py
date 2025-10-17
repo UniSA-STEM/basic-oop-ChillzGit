afed79d7909fea4767f4ae8872e45a128284eafe
Patrick <patrickwilliams707@outlook.com>
2025-10-17 20:18:07 +1030

    Added get_condition function and string conversion method

--- Rig.py
+++ Rig.py
@@ -14,6 +14,27 @@ class Rig:
         self.__damage = 0
         self.__broken = False
         self.__upgrade_level = 0
-        self.__storage = [Asset("Data Spike", "tbd"), Asset("Data Spike", "tbd"),
-                          Asset("Removable Drive", "tbd")]
+        self.__storage = [Asset("Data Spike", "Launch at other rigs to damage"), Asset("Data Spike", "Launch at other rigs to damage"),
+                          Asset("Removable Drive", "Used to extract unsecured assets")]
+
+
+    def get_condition(self):
+        if self.__damage >= 2:
+            self.__broken = True
+            condition = "Broken"
+        elif self.__damage == 1:
+            condition = "Damaged"
+        else:
+            condition = "Broken"
+
+        return f"{condition} (Level {self.__upgrade_level})"
+
+    def __str__(self):
+        condition = self.get_condition()
+        asset_name = ""
+        for asset in self.__storage:
+            asset_name += asset.get_name() + ", "
+        asset_name = asset_name[:-2]
+
+        return f"{self.__name} | {condition} | Storage: {asset_name}"
 
